6f8bcdfe34cfe9b11aced90ed5c29836408eafe9
Zxw854953678 <Zxw854953678@163.com>
2016-07-18 22:44:10 +0800

    add buildItems function test

--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -1,29 +1,112 @@
 'use strict';
 
 describe('pos', () => {
-  let tags;
+  describe('unit test', ()=> {
 
-  beforeEach(() => {
-    tags = [
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000003-2',
-      'ITEM000005',
-      'ITEM000005',
-      'ITEM000005'
-    ];
+    describe('test : buildItems function', ()=> {
+      let tags;
+
+      beforeEach(() => {
+        tags = [
+          'ITEM000001',
+          'ITEM000001',
+          'ITEM000001',
+          'ITEM000001',
+          'ITEM000001',
+          'ITEM000003-2',
+          'ITEM000005',
+          'ITEM000005',
+          'ITEM000005'
+        ];
+      });
+
+      it('print items1', () => {
+        let items = buildItems(['ITEM000001','ITEM000001-2','ITEM000005']);
+
+        const expectItems = [
+          {
+            itemInfo:{
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00},
+            count:3
+          },
+          {
+            itemInfo:{
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.50
+            },
+            count:1
+          }
+        ];
+
+        expect(items).toEqual(expectItems);
+      });
+
+      it('print items2', () => {
+        let items = buildItems(inputs);
+
+        const expectItems = [
+          {
+            itemInfo:{
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00},
+            count:5
+          },
+          {
+            itemInfo:{
+              barcode: 'ITEM000003',
+              name: '荔枝',
+              unit: '斤',
+              price: 15.00
+            },
+            count:2
+          },
+          {
+            itemInfo:{
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.50
+            },
+            count:3
+          }
+        ];
+
+        expect(items).toEqual(expectItems);
+      });
+    });
   });
 
-  it('should print text', () => {
+  describe('integration test', ()=> {
+    let tags;
 
-    spyOn(console, 'log');
+    beforeEach(() => {
+      tags = [
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000003-2',
+        'ITEM000005',
+        'ITEM000005',
+        'ITEM000005'
+      ];
+    });
 
-    printReceipt(tags);
+    it('should print text', () => {
 
-    const expectText = `***<没钱赚商店>收据***
+      spyOn(console, 'log');
+
+      printReceipt(tags);
+
+      const expectText = `***<没钱赚商店>收据***
 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
 名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)
 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
@@ -32,6 +115,7 @@ describe('pos', () => {
 节省：7.50(元)
 **********************`;
 
-    expect(console.log).toHaveBeenCalledWith(expectText);
+      expect(console.log).toHaveBeenCalledWith(expectText);
+    });
   });
 });
